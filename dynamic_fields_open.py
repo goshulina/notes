c24c382f23db4f009e3c2db7749ccc8f04df27cc
Georgy Chernousov <georgi.chernousov@gmail.com>
2022-01-14 13:39:36 +0300

    comments

--- dynamic_fields_open.py
+++ dynamic_fields_open.py
@@ -1,19 +1,22 @@
 import copy as c
-import sys
-from tqdm import tqdm
 import csv
 
-csv.field_size_limit(sys.maxsize)
-csv.field_size_limit(sys.maxsize)
+# Раскладывает колонки csv файла по переменным без знаниния о том, сколько колонок находится в csv файле
+# Если в csv файле 3 колонки то будет возвращен список из 3х списков, где в каждом будут содержаться
+# значения из колонок csv файла
+
 
 class Vars:
     pass
 
+
 def doc_open(path, v, names, delim, threshold):
     members = [attr for attr in dir(v) if not callable(getattr(v, attr)) and not attr.startswith("__")]
+    print(members)
     varss = []
     for i in members:
         varss.append(getattr(v, i))
+    print(varss)
     with open(path, 'rt') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=delim)
         for n, i in enumerate(readCSV):
@@ -26,6 +29,16 @@ def doc_open(path, v, names, delim, threshold):
 
 
 def names_open_hendler(path, names=False, delim=',', threshold=999999):
+    """
+    Раскладывает колонки csv файла по переменным без знаниния о том, сколько колонок находится в csv файле
+    Если в csv файле 3 колонки то будет возвращен список из 3х списков, где в каждом будут содержаться
+    значения из колонок csv файла
+    :param path: path to csv
+    :param names: True if there are names in csv, and their values are not desirreble in output
+    :param delim: csv delimiter
+    :param threshold: how many rows to exctract
+    :return: list of lists. iths item corresponds to iths csv column
+    """
     length = 0
     with open(path, 'rt') as csvfile:
         readCSV = csv.reader(csvfile, delimiter=delim)
@@ -41,5 +54,7 @@ def names_open_hendler(path, names=False, delim=',', threshold=999999):
     return docs
 
 
-path = '.csv'
-x = names_open_hendler(path, threshold=3, names=False, delim=',')
+names_open_hendler('.csv',
+                   threshold=3,
+                   names=True,
+                   delim=';')
